cd5f7928e18acb014f52a34b74d29d59c79dcfc3
Hyun Kim <rlagus7114@gmail.com>
2025-05-21 00:31:38 -1000

    Implement EJS views and layout structure, replacing HTML files with EJS templates

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-// Set up basic route
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('layout', 'layouts/main');
+app.use(expressLayouts);
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Welcome to the Express server!');
+  res.render('index', { 
+    title: 'Bujajjib',
+    activePage: 'home'
+  });
 });
 
-// Start the server
+app.get('/about', (req, res) => {
+  res.render('about', { 
+    title: 'Bujajjib - About',
+    activePage: 'about'
+  });
+});
+
+app.get('/order', (req, res) => {
+  res.render('order', { 
+    title: 'Bujajjib - Order',
+    activePage: 'order'
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 }); 
